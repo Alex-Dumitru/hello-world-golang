a09749dad9cebdf556b9252c3f11fadc54c08c35
Alex Dumitru <dumitru.alex0@gmail.com>
2021-01-10 23:41:19 +0100

    add hello world program

---
+++ hello.go
@@ -0,0 +1,7 @@
+package main // declare a main package
+
+import "fmt" // import fmt package (functions for formatting text, including printing to the console)
+
+func main() { // implement main function. Executes by default
+	fmt.Println("Hello, World!") // print Hello World to console
+}
